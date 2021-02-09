824fb5b8b42fc06da44c6418b21dddac38d5c37e
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2021-02-09 14:31:25 +0100

    Randomly load focus from data/site.yaml

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Yaml\Yaml;
 
 class DefaultController extends BaseController
 {
@@ -16,8 +17,24 @@ class DefaultController extends BaseController
         $this->contentService->setLocale($request->getLocale());
         $volumes = $this->contentService->getVolumes();
 
+        // load the focus
+        $info = Yaml::parseFile($this->dataDir . '/site.yaml');
+        $focus = [];
+        if (is_array($info) && array_key_exists('focus', $info)) {
+            if (is_array($info['focus']) && count($info['focus']) > 0) {
+                $focus = $info['focus'][array_rand($info['focus'])];
+                foreach ($volumes as $volume) {
+                    if ($volume->getId() == $focus['volume']) {
+                        $focus['volume'] = $volume;
+                        break;
+                    }
+                }
+            }
+        }
+
         return $this->render('Default/home.html.twig', [
             'volumes' => $volumes,
+            'focus' => $focus,
         ]);
     }
 }
