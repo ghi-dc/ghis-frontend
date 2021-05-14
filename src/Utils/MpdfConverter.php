c8aec40539b05e061423a019ce1dd98c58a0a861
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2021-05-14 18:01:32 +0200

    Whitespace tweaks

--- src/Utils/MpdfConverter.php
+++ src/Utils/MpdfConverter.php
@@ -51,7 +51,7 @@ extends \Mpdf\Mpdf
     // mpdf
     public function __construct($options = [])
     {
-        // mpdf 7.x
+        // mpdf >= 7.x
         $defaultConfig = (new \Mpdf\Config\ConfigVariables())->getDefaults();
         $fontDir = $defaultConfig['fontDir'];
         /*
@@ -68,12 +68,15 @@ extends \Mpdf\Mpdf
         $options['fontDir'] = array_key_exists('fontDir', $options)
             ? array_merge($options['fontDir'], $fontDir)
             : $fontDir;
+
         $defaultFontConfig = (new \Mpdf\Config\FontVariables())->getDefaults();
         $fontdata = $defaultFontConfig['fontdata'];
         $options['fontdata'] = array_key_exists('fontdata', $options)
             ? $fontdata + $options['fontdata']
             : $fontdata;
+
         parent::__construct($options);
+
         $this->autoScriptToLang = true;
         $this->SetDisplayMode('fullpage');
     }
