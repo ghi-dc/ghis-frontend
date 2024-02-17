2bdd2cb5147d3d7eb3dcc64192f06ed1729f860c
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2024-02-17 14:04:16 +0100

    Avoid error in Symfony 6: Input value contains a non-scalar value

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -198,7 +198,12 @@ class SearchController extends BaseController
                 $q = trim($request->query->get('q'));
             }
 
-            $filter = $request->query->get('filter');
+            $queryAll = $request->query->all();
+
+            $filter = array_key_exists('filter', $queryAll)
+                ? $queryAll['filter']
+                : [];
+
             if (!empty($filter)) {
                 // filter down to allowed facetNames as keys
                 $filter = array_intersect_key($filter, array_flip($facetNames));
