1c605bd61d6cb90794298241c563bbc61f371b99
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2025-03-01 11:36:56 +0100

    composer require --dev friendsofphp/php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,25 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude([
+        'vendor',
+        'config',
+        'var',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        '@PER-CS' => true,
+        '@PHP82Migration' => true,
+        'control_structure_continuation_position' => ['position' => 'next_line'],
+        'elseif' => false, // don't change else if to elseif
+        'visibility_required' => ['elements' =>
+            // disable changing var into public
+            [/*'const', 'method', 'property'*/]
+        ],
+    ])
+    ->setFinder($finder)
+;
