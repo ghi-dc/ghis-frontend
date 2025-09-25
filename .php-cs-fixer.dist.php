6f7327b3928c023b3d4c00272e0d8d70a89ac130
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2025-09-25 13:24:51 +0200

    composer update rename visibility_required into modifier_keywords

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -17,7 +17,7 @@ return (new PhpCsFixer\Config())
         '@PHP82Migration' => true,
         'control_structure_continuation_position' => ['position' => 'next_line'],
         'elseif' => false, // don't change else if to elseif
-        'visibility_required' => ['elements' =>
+        'modifier_keywords' => ['elements' =>
             // disable changing var into public
             [/*'const', 'method', 'property'*/]
         ],
